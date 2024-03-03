7e8cdb6a8569097196ed7e97fafc26d7c14a4b5b
Kacper Tryniecki <kacper@tryniecki.com>
2024-03-03 22:34:18 +0100

    revert to .net 8

--- GotikAniki69.Server/Models/ResponseModel.cs
+++ GotikAniki69.Server/Models/ResponseModel.cs
@@ -5,19 +5,31 @@ namespace GotikAniki69.Server.Models;
 public class ResponseModel
 {
     [JsonPropertyName("type")]
-    public required string Type { get; set; }
+    public required string Type
+    {
+        get; set;
+    }
 
     [JsonPropertyName("payload")]
-    public PayloadModel? Payload { get; set; }
+    public PayloadModel? Payload
+    {
+        get; set;
+    }
 }
 
 public class PayloadModel
 {
     [JsonPropertyName("index")]
-    public int Index { get; set; }
+    public int Index
+    {
+        get; set;
+    }
 
     [JsonPropertyName("nick")]
-    public string? NickName { get; set; }
+    public string? NickName
+    {
+        get; set;
+    }
 
     [JsonPropertyName("skinId")]
     public string? SkinId
@@ -26,8 +38,14 @@ public class PayloadModel
     }
 
     [JsonPropertyName("x")]
-    public double X { get; set; }
+    public double X
+    {
+        get; set;
+    }
 
     [JsonPropertyName("y")]
-    public double Y { get; set; }
+    public double Y
+    {
+        get; set;
+    }
 }
