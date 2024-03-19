17f49374164a276c03602e89b98608030e49b377
Kacper Tryniecki <kacper@tryniecki.com>
2024-03-19 18:20:50 +0100

    Change address

--- GotikAniki69.Server/Program.cs
+++ GotikAniki69.Server/Program.cs
@@ -23,9 +23,9 @@ public static class Program
     private static async Task Initialize()
     {
         var httpListener = new HttpListener();
-        httpListener.Prefixes.Add("http://gotikaniki69.com:8081/");
+        httpListener.Prefixes.Add("http://test.gotikaniki69.com:8081/");
         httpListener.Start();
-        Console.WriteLine("WebSocket server started at ws://gotikaniki69.com:8081/");
+        Console.WriteLine("WebSocket server started at ws://test.gotikaniki69.com:8081/");
 
         while (true)
         {
